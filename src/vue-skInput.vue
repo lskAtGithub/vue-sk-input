56eaa4fb729d5ce571554a0cc03d095942ab3768
lsk <949919743@qq.com>
2021-05-06 18:33:21 +0800

    初步样式，基本属性给定

--- src/vue-skInput.vue
+++ src/vue-skInput.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-    {{test || 'no test props'}}
+  <div class="sk__input_box">
+    <input
+      class="sk__input"
+      :class="{'errorInput': verify}"
+      :value="value"
+      :maxlength="maxlength"
+      :minlength="minlength"
+      :autocomplete="autocomplete"
+      :placeholder="placeholder"
+      @input="handleInput"
+      @focus="handleFocus"
+      @blur="handleBlur"
+    />
+
   </div>
 </template>
 
@@ -8,11 +20,48 @@
   export default {
     name: 'SkInput',
     props:{
-      test: {}
+      value: [Number, String],
+      maxlength: [Number, String],
+      minlength: [Number, String],
+      placeholder: [Number, String],
+      autocomplete: {
+        type: String,
+        default: 'off'
+      },
+    },
+    data(){
+      return {
+        verify: true
+      }
+    },
+    methods: {
+      handleInput(event){
+        this.$emit('input', event.target.value)
+      },
+      handleFocus(event){
+        this.$emit('focus', event)
+      },
+      handleBlur(event){
+        this.$emit('blur', event)
+      }
     }
   }
 </script>
 
 <style scoped>
-
+.sk__input_box{
+  width: 100%;
+  min-width: 180px;
+}
+.sk__input_box .sk__input{
+  outline: none;
+  width: 100%;
+  border: 1px solid #dcdfe6;
+  color: #606266;
+  font-size: 15px;
+  padding: 5px 14px;
+}
+.sk__input_box .sk__input::placeholder{
+  color: #c0cfe3;
+}
 </style>
